d5ab658b6219f13de7e19b54e9308b2cc11211f9
Rafal Wrobel <wrobelrafal1992@gmail.com>
2018-05-09 00:06:24 +0200

    Fix

--- js/Column.js
+++ js/Column.js
@@ -7,14 +7,14 @@ function Column(id, name) {
 
 	function createColumn() {
 		// TWORZENIE NOWYCH WĘZŁÓW
-		var column = $('<div class="column"></div>');
-		var columnTitle = $('<h2 class="column-title">' + self.name + '</h2>');
-		var columnCardList = $('<ul class="card-list"></ul>');
-		var columnDelete = $('<button class="btn-delete">x</button>');
-		var columnAddCard = $('<button class="column-add-card">Dodaj kartę</button>');
+		var $column = $('<div class="column"></div>');
+		var $columnTitle = $('<h2 class="column-title">' + self.name + '</h2>');
+		var $columnCardList = $('<ul class="card-list"></ul>');
+		var $columnDelete = $('<button class="btn-delete">x</button>');
+		var $columnAddCard = $('<button class="column-add-card">Dodaj kartę</button>');
 		
 		// PODPINANIE ODPOWIEDNICH ZDARZEŃ POD WĘZŁY
-		columnDelete.click(function() {
+		$columnDelete.click(function() {
 			self.deleteColumn();
 		});
 		
@@ -36,11 +36,11 @@ function Column(id, name) {
 		});
 			
 			// KONSTRUOWANIE ELEMENTU KOLUMNY
-		column.append(columnTitle)
-			.append(columnDelete)
-			.append(columnAddCard)
-			.append(columnCardList);
-			return column;
+		$column.append($columnTitle)
+			.append($columnDelete)
+			.append($columnAddCard)
+			.append($columnCardList);
+			return $column;
 		}
 	}
 Column.prototype = {
